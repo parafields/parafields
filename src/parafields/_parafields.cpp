2a653132693e56f2a0e4c03239069be2b0fd03de
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-09-09 11:33:14 +0200

    Simple RandomField class binding

--- src/parafields/_parafields.cpp
+++ src/parafields/_parafields.cpp
@@ -45,6 +45,19 @@ PYBIND11_MODULE(_parafields, m)
     Dune::RandomField::RandomField<GridTraits<double, double, 2>>;
   py::class_<RandomField2D> field2d(m, "RandomField2D");
   field2d.def(py::init<Dune::ParameterTree>());
+
+  // The (lazy) method for random field generation
+  field2d.def("generate", [](RandomField2D& self) { self.generate(); });
+
+  // A method to evaluate at a given coordinate
+  field2d.def("probe",
+              [](const RandomField2D& self, const py::array_t<double>& pos) {
+                Dune::FieldVector<double, 1> out;
+                Dune::FieldVector<double, 2> apos;
+                std::copy(pos.data(), pos.data() + 2, apos.begin());
+                self.evaluate(apos, out);
+                return out[0];
+              });
 }
 
 } // namespace parafields
