dd755c35dfffea935c2fcffe1092d0dc0eedadd5
Dominic Kempf <dokempf@users.noreply.github.com>
2022-09-14 10:46:00 +0200

    Add FakeMPI fallback to build a sequential wheel (#2)

--- test/conftest.py
+++ test/conftest.py
@@ -1,1 +1,7 @@
-from mpi4py import MPI
+# If parafields is operating in parallel, we need to initialize
+# MPI for the testing. If it is operating in sequential, this will
+# throw an ImportError that we do not care about.
+try:
+    from mpi4py import MPI
+except ImportError:
+    pass
